6d934a02837eaabf3ecff60ea42f3450f63c7b16
gustavomanuel <gustavogalveias@campus.ul.pt>
2021-10-03 23:03:09 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const[name, setName] = useState('Gustavo')
+
+  const handleClick = () => {
+    if (name == 'Gustavo'){
+      setName('Manuel')
+    }
+    else{
+      setName('Gustavo')
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>This is just a sample text</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.testText}>Babinga</Text>
+        <Text style={styles.testText}>My name is {name}</Text>
+
+        <View style={styles.buttonContainer}>
+          <Button title='Press me to change name.' onPress={handleClick}/> 
+
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -14,8 +37,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#99ffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  body: {
+    flex: 1,
+    backgroundColor: '#ff0fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+
+  header: {
+    padding: 20,
+    backgroundColor: '#00ffff',
+  },
+
+  testText: {
+    fontSize: 30,
+    color: '#ffffff',
   },
 });
